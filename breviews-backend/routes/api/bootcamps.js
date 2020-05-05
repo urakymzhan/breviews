7ae8f231d6715630e7e3aff12e22eeb55875f97f
azamatrashidbekov <azamat.rashidbekov@gmail.com>
2020-05-05 15:55:50 -0500

    footer and favicon upd

--- breviews-backend/routes/api/bootcamps.js
+++ breviews-backend/routes/api/bootcamps.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
 const Bootcamp = require('../../models/Bootcamps');
 
 // @route  GET api/landing
