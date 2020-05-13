7fbc683e64ca42b18315e17b5a4b0f82738ddb79
askar7 <31324870+askar7@users.noreply.github.com>
2020-05-13 15:39:46 -0500

    added Blog component

---
+++ breviews-client/src/components/pages/Blog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './style/blog.css'
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    
+     <div>This is Blog</div>
+    );
+  }
+}
+
+export default Blog;
