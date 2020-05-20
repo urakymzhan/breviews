3245a4655d1c124c8207e600886ff4218720ada5
makhabatmaksatbekova <59904300+makhabatmaksatbekova@users.noreply.github.com>
2020-05-20 16:43:55 -0700

    fetch upstream

--- breviews-client/src/components/B_Molecules/Footer.jsx
+++ breviews-client/src/components/B_Molecules/Footer.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import './style/footer.css'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer style={style.footerWrapper}>
       <div>
         <p>© 2020 BootcampAvenue</p> 
-        <span>About</span> {" "}
-        <span>Terms</span>
+        <ul id="logo-wrapper" style={style.ul}>
+          <li id="footer-about" style={style.lists} >
+          <Link to="/team" style={style.links}>
+              About
+           </Link>
+          </li>
+          <li id="footer-terms" style={style.lists}>
+          <Link to="/legal" style={style.links}>
+              Terms
+            </Link>
+          </li>
+        </ul>
+
+        {/* /* <span>About</span> {" "} */}
+        {/* {<span>Terms</span>} */}
       </div>
     </footer>
   );
@@ -17,6 +31,9 @@ export default Footer;
 
 // STYLES:
 const style = {
-  footerWrapper: {  padding: "2em", background: "#788b95", fontSize: "12px", color: "#fff", textAlign: "center",}
+  footerWrapper: {  padding: "2em", background: "#788b95", fontSize: "12px", color: "#fff", textAlign: "center",},
+  ul: { listStyle: "none", fontSize: "12px" },
+  lists: { display: "inline-block", padding: "7px"},
+  links: { textDecoration: "none", color: "inherit"}
 }
 
--- breviews-client/src/components/B_Molecules/Header.jsx
+++ breviews-client/src/components/B_Molecules/Header.jsx
@@ -3,64 +3,102 @@ import { Link } from "react-router-dom";
 import brlogo from '../../../public/assets/brlogo3.png'
 import { withRouter } from "react-router";
 import PropTypes from "prop-types";
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
-class Header extends React.Component {
-  static propTypes = {
-    location: PropTypes.object.isRequired,
-  };
+const Header = (props) => {
+  return (
+    <div>
+      <Nav pills>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Another Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink disabled href="#">Disabled Link</NavLink>
+        </NavItem>
+      </Nav>
+    </div>
+  );
+}
 
-  render() {
-    const { location } = this.props;
-    const bgcolor = location.pathname === '/' || location.pathname === '/results' ? { background: '#d7d7d7'} : { background: '#fff'};
-    return (
-      <header style={bgcolor}>
-        <nav id="header-nav" style={style.nav}>
-        <ul id="logo-wrapper" style={style.ul}>
-          <li id="header-logo" style={{float: "left", margin: "0.2em 1.2em auto auto" }}>
-            <Link to="/" style={style.links}>
-              <img
-                id="brlogo"
-                src={brlogo}
-                style={{ width: "auto", height: "30px"}}
-              />
-            </Link>
-          </li>
-          <li id="header-logo" style={style.lists}>
-            <Link to="/results" style={style.links}>
-              Leave a review
-            </Link>
-          </li>
-        </ul>
-        <ul style={style.ul}>
-          <li id="header-team" style={style.lists}>
-            <Link to="/patreon" style={style.links}>
-              Patreon
-            </Link>
-          </li>
-          <li id="header-team" style={style.lists}>
-            <Link to="/team" style={style.links}>
-              About
-            </Link>
-          </li>
-          <li id="header-team" style={style.lists}>
-            <Link to="/blog" style={style.links}>
-              Blog
-            </Link>
-          </li>
-          <li id="header-contact" style={style.lists} >
-            <Link to="/contact" style={style.links}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      </header>
-    );  
-  }
-};
+export default Header;
 
-const FlexibleHeader = withRouter(Header);
-export default FlexibleHeader;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Header extends React.Component {
+//   static propTypes = {
+//     location: PropTypes.object.isRequired,
+//   };
+
+//   render() {
+//     const { location } = this.props;
+//     const bgcolor = location.pathname === '/' || location.pathname === '/results' ? { background: '#d7d7d7'} : { background: '#fff'};
+//     return (
+//       <header style={bgcolor}>
+//         <nav id="header-nav" style={style.nav}>
+//         <ul id="logo-wrapper" style={style.ul}>
+//           <li id="header-logo" style={{float: "left", margin: "0.2em 1.2em auto auto" }}>
+//             <Link to="/" style={style.links}>
+//               <img
+//                 id="brlogo"
+//                 src={brlogo}
+//                 style={{ width: "auto", height: "30px"}}
+//               />
+//             </Link>
+//           </li>
+//           <li id="header-logo" style={style.lists}>
+//             <Link to="/results" style={style.links}>
+//               Leave a review
+//             </Link>
+//           </li>
+//         </ul>
+//         <ul style={style.ul}>
+//           <li id="header-team" style={style.lists}>
+//             <Link to="/patreon" style={style.links}>
+//               Patreon
+//             </Link>
+//           </li>
+//           <li id="header-team" style={style.lists}>
+//             <Link to="/team" style={style.links}>
+//               About
+//             </Link>
+//           </li>
+//           <li id="header-team" style={style.lists}>
+//             <Link to="/blog" style={style.links}>
+//               Blog
+//             </Link>
+//           </li>
+//           <li id="header-contact" style={style.lists} >
+//             <Link to="/contact" style={style.links}>
+//               Contact
+//             </Link>
+//           </li>
+//         </ul>
+//       </nav>
+//       </header>
+//     );  
+//   }
+// };
+
+// const FlexibleHeader = withRouter(Header);
+// export default FlexibleHeader;
 
 // STYLES:
 const style = {
